72ffba5dcb24e0ba14d310fac1032eae18d31823
qiaoxiuxia <15680879850@163.com>
2023-03-04 11:34:15 +0800

    StudentDao增加删除方法

--- ssm-dao/src/main/java/com/itqiao/dao/StudentDao.java
+++ ssm-dao/src/main/java/com/itqiao/dao/StudentDao.java
@@ -17,7 +17,11 @@ public interface StudentDao {
     //查询所有学生
     @Select("select * from student")
     List<Student> findAll();
+
     //添加学生
     @Insert("insert into student values (null,#{name},#{sex},#{address})")
     void add(Student student);
+
+
+    void delete(Long id);
 }
